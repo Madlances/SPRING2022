678f364565a2343e4709cb2322e0880188219660
kassimadsen <madsenkassi@gmail.com>
2022-02-14 16:12:21 -0700

    started next projecft 3660

---
+++ CS3660/Framework 1-LocalStorageService/team_data.js
@@ -0,0 +1,45 @@
+ var teamData = {
+     app: {
+      sortCol:"name",
+      sortDir:"asc"
+     },
+     data: [
+       {
+         id: 1,
+         name:"Raptors",
+         coachId: 1,
+         coachFirst: "Ken",
+         coachLast: "jenson",
+         coachPhone: "801-333-4444",
+         coachEmail: "ken.jenson@uvu.edu",
+         coachLicenseLevel: 1,
+         league: 1,
+         division: 1
+       },
+        {
+         id: 2,
+         name:"Killer Bunnies",
+         coachId: 2,
+         coachFirst: "Peter",
+         coachLast: "Rabbit",
+         coachPhone: "801-333-4444",
+         coachEmail: "peter.rabbit@uvu.edu",
+         coachLicenseLevel: 1,
+         league: 1,
+         division: 2
+       },
+       {
+         id: 3,
+         name:"Thunderbirds",
+         coachId: 3,
+         coachFirst: "Harry",
+         coachLast: "DirtyDog",
+         coachPhone: "801-333-4444",
+         coachEmail: "harry.dirty.dog@uvu.edu",
+         coachLicenseLevel: 2,
+         league: 1,
+         division: 2
+       }
+     ]
+     
+   }
