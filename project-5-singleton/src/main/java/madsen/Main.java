0ac7a15e369d8c1294e9b7f6ef6083616e13b749
kassimadsen <madsenkassi@gmail.com>
2022-02-15 12:08:02 -0700

    added comments and uml

--- project-5-singleton/src/main/java/madsen/Main.java
+++ project-5-singleton/src/main/java/madsen/Main.java
@@ -1,9 +1,21 @@
 package madsen;
 
+/**
+ * This is our main driver class.
+ * This class will let the user input a Policy Holder Name and ID.
+ * Then we will use the singleton class Policy to print out the users
+ * Holder name and id and the description of the insurances policy.
+ */
+
 import java.util.Scanner;
 
 public class Main 
 {
+    /**
+     * This function will get user input and print out
+     * what the user has put in and the policy description.
+     * @param args
+     */
     public static void main( String[] args ) {
         Policy policy = Policy.getInstance();
         System.out.println("Welcome to our Insurance Program!");
--- project-5-singleton/src/main/java/madsen/Policy.java
+++ project-5-singleton/src/main/java/madsen/Policy.java
@@ -1,5 +1,9 @@
 package madsen;
 
+/**
+ * This is our Policy class that implements the Singleton pattern.
+ */
+
 public class Policy {
     private static Policy uniqueInstance;
 
@@ -10,6 +14,12 @@ public class Policy {
         }
     };
 
+    /**
+     * this function will instantiate the Policy,
+     * if uniqueInstance is null.
+     * We are also using lazy loading and thread safety in this function.
+     * @return
+     */
     public static Policy getInstance() {
         if (uniqueInstance == null) {
             synchronized (Policy.class) {
@@ -21,22 +31,48 @@ public class Policy {
         return uniqueInstance;
     }
 
+    /**
+     * This function will return the description of the Policy
+     * @return
+     */
     public String getDescription() {
         return "Our Policy covers Health, Dental, and Medical.";
     }
 
+    /**
+     * This function will return the Policy Holder name
+     * @param name
+     * @return
+     */
     public String getPolicyHolderName(String name) {
         return name;
     }
 
+    /**
+     * This function will return the ID
+     * @param id
+     * @return
+     */
     public String getPolicyID(String id) {
         return id;
     }
 
+    /**
+     * This function will set the Policy Holder Name so that
+     * we will be able to return it when we call getPolicyHolderName
+     * @param name
+     * @return
+     */
     public String setPolicyHolderName(String name) {
         return getPolicyHolderName(name);
     }
 
+    /**
+     * This function will set the Policy ID so that
+     * we will be able to return it in getPolicyID 
+     * @param id
+     * @return
+     */
     public String setPolicyID(String id) {
         return getPolicyID(id);
     }
